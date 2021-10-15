7270c4b77ce084d92c9f0e68603498c2d3408ee6
belha <42bhamdi@gmail.com>
2021-10-15 15:14:57 +0200

    2

--- app/src/main/java/com/samuelvialle/plusoumoins/Splash.java
+++ app/src/main/java/com/samuelvialle/plusoumoins/Splash.java
@@ -2,18 +2,28 @@ package com.samuelvialle.plusoumoins;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 public class Splash extends AppCompatActivity {
-
+ImageView ivLoading;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash2);
+        setContentView(R.layout.activity_splash);
 
         ImageView imageView = findViewById(R.id.ivLoading);
 
-        imageView.animate().alpha(1).rotation(6300).setDuration(2000);
+        imageView.animate().alpha(1).rotation(3600).setDuration(2000);
+        imageView.animate().alpha(2).translationY(-1000).setDuration(5000);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Splash.this,Accueil.class);
+                startActivity(intent);
+            }
+        });
     }
 }
